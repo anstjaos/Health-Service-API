9b0e7b87e2ec47f9e7e73fb1cbc32ab1a1b5f147
moonseo.kim <moonseo.kim@nhn.com>
2021-05-20 20:21:51 +0900

    init project

---
+++ src/main/java/com/heath/service/api/healthserviceapi/HealthServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.heath.service.api.healthserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthServiceApiApplication.class, args);
+	}
+
+}
