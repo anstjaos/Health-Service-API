27406919a9bf5c5310e621c55c4b714954a41a0a
moonseo.kim <moonseo.kim@nhn.com>
2021-05-20 21:06:22 +0900

    add project package

--- src/main/java/com/heath/service/api/HealthServiceApiApplication.java
+++ src/main/java/com/heath/service/api/HealthServiceApiApplication.java
@@ -1,4 +1,4 @@
-package com.heath.service.api.healthserviceapi;
+package com.heath.service.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
