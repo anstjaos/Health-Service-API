7fbd6db78676a1d0fdd865081ceab0d590ecc7c9
moonseo.kim <moonseo.kim@nhn.com>
2021-05-20 21:22:26 +0900

    set test configuration

---
+++ src/test/java/com/heath/service/api/DbUnitTestContext.java
@@ -0,0 +1,19 @@
+package com.heath.service.api;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("h2db")
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DbUnitTestExecutionListener.class
+})
+public abstract class DbUnitTestContext {
+}
